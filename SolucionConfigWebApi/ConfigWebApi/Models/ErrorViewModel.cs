8777e00495a512582c91f15af33df52258ab7e5d
JoseJimenez01 <jg3633110@gmail.com>
2024-06-23 10:33:13 -0600

    Creacion de la plantilla
    Se hizo el proyecto para empezar la configuracion

---
+++ SolucionConfigWebApi/ConfigWebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConfigWebApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
